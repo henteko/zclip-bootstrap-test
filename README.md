9385ae0c45a6178bffa4ca17ba14a919cb336824
henteko <henteko07@gmail.com>
2013-07-18 23:42:58 +0900

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+jQuery 1.10.2  
+jQuery ZeroClipboard 1.1.1
+Twitter Bootstrap 2.3.2 
---
+++ assets/main.js
@@ -0,0 +1,26 @@
+$(function() {
+  var $button = $('#copy-button');
+  $button.updateTooltip = function(title) {
+    return this
+    .attr('title', title)
+    .tooltip('fixTitle');
+  };
+
+  $button.updateTooltip($button.data('before'))
+    .zclip({
+      path: $button.data('movie'),
+      copy: function() {
+        return $('#copy-text').val();
+      },
+      afterCopy: function(){
+        $button
+          .updateTooltip($button.data('after'))
+          .tooltip('show');
+        setTimeout(function(){
+          $button
+            .tooltip('hide')
+            .updateTooltip($button.data('before'));
+        }, 1000);
+      }
+    });
+});
